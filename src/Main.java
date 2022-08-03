e35b1f8587baad0037f3ff0812a3a4d7940cfa15
java-13-7-2022-Frantisek-T <ift_2020@volny.cz>
2022-08-03 21:12:14 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import Prodejce.Prodejce;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Prodejce prodejce = new Prodejce();
+
+        double prumerneMnozstviProdaneMrkveNaJednuSmlouvu = 0.0d;
+
+        if (! (prodejce.getProdejcePocetSjednanychSmluv()==0)) {
+            prumerneMnozstviProdaneMrkveNaJednuSmlouvu = prodejce.getProdejceCelkoveMnozstvíProdaneMrkve() / prodejce.getProdejcePocetSjednanychSmluv();
+        }
+
+    System.out.println("Průměrné množství prodané mrkve na jednu smlouvu: " + prumerneMnozstviProdaneMrkveNaJednuSmlouvu + " tun.");
+
+    }
+}
---
+++ src/Prodejce/Prodejce.java
@@ -0,0 +1,95 @@
+package Prodejce;
+
+import java.time.LocalDate;
+
+public class Prodejce {
+
+    String      prodejceJmenoPrijmeni                               = "Jan Prodavač";
+    LocalDate   prodejceDatumNarození                               = LocalDate.of(1963,2,1);
+    int         prodejcePocetSjednanychSmluv                        = 12345;
+    double      prodejceCelkoveMnozstvíProdaneMrkve                 = 87654.321;                                    // v tunách
+    String      prodejceSídloNazevMesta                             = "Mrkvičkov";
+    String      prodejceVozidloRegistracniZnacka                    = "1AM 0833";
+    double      prodejceVozidloSpotreba                             = 9.87;                                         // v litrech na 100 km
+    String      prodejcePocitacAdresaIP                             = "001.002.003.004";                            // adresa verze 4
+    double      vypsatPrumerneMnozstviProdaneMrkveNaJednuSmlouvu    = 0.0d;
+
+//    if (prodejcePocetSjednanychSmluv==0) {} else vypsatPrumerneMnozstvíProdaneMrkveNaJednuSmlouvu() = prodejceCelkoveMnozstviProdaneMrkve() / prodejcePocetSjednanychSmluv();
+
+//    System.out.println("Průměrné množství prodané mrkve na jednu smlouvu: " + vypsatPrumerneMnozstviProdaneMrkveNaJednuSmlouvu);
+
+    public String getProdejceJmenoPrijmeni() {
+        return prodejceJmenoPrijmeni;
+    }
+
+    public void setProdejceJmenoPrijmeni(String prodejceJmenoPrijmeni) {
+        this.prodejceJmenoPrijmeni = prodejceJmenoPrijmeni;
+    }
+
+    public LocalDate getProdejceDatumNarození() {
+        return prodejceDatumNarození;
+    }
+
+    public void setProdejceDatumNarození(LocalDate prodejceDatumNarození) {
+        this.prodejceDatumNarození = prodejceDatumNarození;
+    }
+
+    public int getProdejcePocetSjednanychSmluv() {
+        return prodejcePocetSjednanychSmluv;
+    }
+
+    public void setProdejcePocetSjednanychSmluv(int prodejcePocetSjednanychSmluv) {
+        this.prodejcePocetSjednanychSmluv = prodejcePocetSjednanychSmluv;
+    }
+
+    public double getProdejceCelkoveMnozstvíProdaneMrkve() {
+        return prodejceCelkoveMnozstvíProdaneMrkve;
+    }
+
+    public void setProdejceCelkoveMnozstvíProdaneMrkve(double prodejceCelkoveMnozstvíProdaneMrkve) {
+        this.prodejceCelkoveMnozstvíProdaneMrkve = prodejceCelkoveMnozstvíProdaneMrkve;
+    }
+
+    public String getProdejceSídloNazevMesta() {
+        return prodejceSídloNazevMesta;
+    }
+
+    public void setProdejceSídloNazevMesta(String prodejceSídloNazevMesta) {
+        this.prodejceSídloNazevMesta = prodejceSídloNazevMesta;
+    }
+
+    public String getProdejceVozidloRegistracniZnacka() {
+        return prodejceVozidloRegistracniZnacka;
+    }
+
+    public void setProdejceVozidloRegistracniZnacka(String prodejceVozidloRegistracniZnacka) {
+        this.prodejceVozidloRegistracniZnacka = prodejceVozidloRegistracniZnacka;
+    }
+
+    public double getProdejceVozidloSpotreba() {
+        return prodejceVozidloSpotreba;
+    }
+
+    public void setProdejceVozidloSpotreba(double prodejceVozidloSpotreba) {
+        this.prodejceVozidloSpotreba = prodejceVozidloSpotreba;
+    }
+
+    public String getProdejcePocitacAdresaIP() {
+        return prodejcePocitacAdresaIP;
+    }
+
+    public void setProdejcePocitacAdresaIP(String prodejcePocitacAdresaIP) {
+        this.prodejcePocitacAdresaIP = prodejcePocitacAdresaIP;
+    }
+
+    public double getVypsatPrumerneMnozstviProdaneMrkveNaJednuSmlouvu() {
+        return vypsatPrumerneMnozstviProdaneMrkveNaJednuSmlouvu;
+    }
+
+    public void setVypsatPrumerneMnozstviProdaneMrkveNaJednuSmlouvu(double vypsatPrumerneMnozstviProdaneMrkveNaJednuSmlouvu) {
+        this.vypsatPrumerneMnozstviProdaneMrkveNaJednuSmlouvu = vypsatPrumerneMnozstviProdaneMrkveNaJednuSmlouvu;
+    }
+}
+
+
+
